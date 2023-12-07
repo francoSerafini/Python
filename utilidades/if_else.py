089ebdccabc00d645297ff904245b0833d42bb13
francoSerafini <frankitooserafini493@gmail.com>
2023-12-06 22:13:13 -0300

    metodos

---
+++ utilidades/if_else.py
@@ -0,0 +1,11 @@
+edad= 14
+
+if edad > 18:
+    print('mayor de edad')
+
+elif edad == 5:
+    print('pequeno')
+    
+else:
+    print('menor de edad')
+    
---
+++ utilidades/metodos_string.py
@@ -0,0 +1,15 @@
+cadena = 'Me llamo Franco'
+cadena1 = 'esta casi TODO en Minuscula'
+
+#print(dir(cadena)) #Muestra los metodos disponibles para las cadenas.
+
+print(cadena.upper()) #Convierte a mayuscula.
+
+print(cadena.lower()) #Convierte a minuscula.
+
+print(cadena1.capitalize()) #Pasa todo a minuscula y la primer letra a mayuscula.
+
+print(cadena.find('z')) #Devuelve la posicion donde se encutra el caracter. -1 si no lo encuentra.
+
+print(cadena.index('e')) #Igual que el find pero devuelve una excepcion en caso de no encotrarlo.
+
