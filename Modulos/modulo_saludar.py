a468fd446ad799549d8213c7f2eb893b87b9d9c6
Franco <frankitooserafini493@gmail.com>
2024-01-25 13:25:50 -0300

    modulos_1

---
+++ Modulos/modulo_saludar.py
@@ -0,0 +1,5 @@
+def saludar(name):
+    return(f"Hola {name}, como estas?")
+    
+saludo = saludar("Franco")
+print(saludo)
---
+++ Modulos/modulos.py
@@ -0,0 +1,5 @@
+import modulo_saludar
+
+saludo = modulo_saludar.saludar("Marcos")
+
+print(saludo)
